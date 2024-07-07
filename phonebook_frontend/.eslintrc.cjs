12d76da7a367e8871e83035225398249d37f8ed2
a21dariodr <a21dariodr@iessanclemente.net>
2024-07-07 14:44:23 +0200

    Solved error

--- phonebook_frontend/.eslintrc.cjs
+++ phonebook_frontend/.eslintrc.cjs
@@ -1,49 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        '@stylistic/jsx'
-    ],
-    "rules": {
-        '@stylistic/jsx/jsx-indent': [
-            'error',
-            2
-        ],
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-uses-react": "off",
-        "react/prop-types": "off",
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
-        ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
-        ],
-        'no-console': 0
-    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@stylistic/jsx'],
+  rules: {
+    '@stylistic/jsx/jsx-indent': ['error', 2],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+  },
 }
--- phonebook_frontend/src/services/db.js
+++ phonebook_frontend/src/services/db.js
@@ -3,26 +3,26 @@ import axios from 'axios'
 const baseUrl = '/api/persons'
 
 const getAllPersons = async () => {
-    const response = await axios.get(baseUrl)
-    console.log('Data fetched succesfully', response.data)
-    return response.data
+  const response = await axios.get(baseUrl)
+  console.log('Data fetched succesfully', response.data)
+  return response.data
 }
 
 const createPerson = async person => {
-    const response = await axios.post(baseUrl, person)
-    console.log('New person added succesfully', response.data)
-    return response.data
+  const response = await axios.post(baseUrl, person)
+  console.log('New person added succesfully', response.data)
+  return response.data
 }
 
 const editPerson = async (personId, updatedPerson) => {
-    const response = await axios.put(`${baseUrl}/${personId}`, updatedPerson)
-    console.log('Person updated succesfully', response.data)
-    return response.data
+  const response = await axios.put(`${baseUrl}/${personId}`, updatedPerson)
+  console.log('Person updated succesfully', response.data)
+  return response.data
 }
 
 const deletePerson = async personId => {
-    const response = await axios.delete(`${baseUrl}/${personId}`)
-    console.log(`Person with id ${personId} deleted succesfully`, response.data)
+  const response = await axios.delete(`${baseUrl}/${personId}`)
+  console.log(`Person with id ${personId} deleted succesfully`, response.data)
 }
 
 export default { getAllPersons, createPerson, editPerson, deletePerson }
